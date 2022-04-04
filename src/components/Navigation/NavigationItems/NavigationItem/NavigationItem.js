6f98545ec2ebb0bec88a46099b5ad750f32117e3
Nwafor Jeremiah <email>
2022-04-04 08:31:24 +0100

    initial commit

---
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+import './NavigationItem.css';
+
+const navigationItem = ( props ) => (
+    <li className="NavigationItem">
+        <NavLink 
+            to={props.link} 
+            activeClassName="active"
+            exact={props.exact} 
+            // className={props.active ? "active" : null}
+        >
+            {props.children}</NavLink>
+    </li>
+);
+
+export default navigationItem;
